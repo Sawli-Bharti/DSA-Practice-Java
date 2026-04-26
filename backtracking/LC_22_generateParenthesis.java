38a13c1354316cb74e78c0f70e60a00dd5b0e205
Bharti-Sawli <16bharti10@gmail.com>
2026-04-26 23:09:41 +0530

    solved my first backtracking approach based dsa problem

---
+++ backtracking/LC_22_generateParenthesis.java
@@ -0,0 +1,25 @@
+/*
+    https://leetcode.com/problems/generate-parentheses/description/
+*/
+
+class Solution {
+    
+
+    public void generate(String current, int open, int close, int n, List<String> result){
+        if(current.length()==2*n){
+            result.add(current);
+            return ;
+        }
+        if(open<n){
+            generate(current+"(",open+1,close,n,result);
+        }
+        if(close<open){
+            generate(current+")",open,close+1,n,result);
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> result=new ArrayList<>();
+        generate("",0,0,n,result);
+        return result;
+    }
+}
