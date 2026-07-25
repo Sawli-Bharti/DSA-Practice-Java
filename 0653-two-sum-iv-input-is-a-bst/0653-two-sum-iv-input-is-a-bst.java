18afd36e58a828145d7b76cac2a764bbf71c1e20
Sawli Bharti <bhartisawli9@gmail.com>
2026-07-25 12:42:39 +0530

    Time: 3 ms (73.91%), Space: 47.3 MB (55.79%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,12 +14,12 @@
  * }
  */
 class Solution {
-    HashMap<Integer,Integer> map=new HashMap<>();
+    Set<Integer> map=new HashSet<>();
     public boolean findTarget(TreeNode root, int k) {
         if(root==null) return false;
-        if(map.containsKey(k-root.val)) return true;
+        if(map.contains(k-root.val)) return true;
 
-        map.put(root.val,0);
+        map.add(root.val);
         return findTarget(root.left,k) || findTarget(root.right,k);
     }
 }
