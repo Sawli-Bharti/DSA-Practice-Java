23be3c9ef0a373e17229fdff3d175c128dd62a79
Sawli Bharti <bhartisawli9@gmail.com>
2026-07-28 23:25:52 +0530

    Time: 1 ms (82.48%), Space: 43.8 MB (60.26%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -14,49 +14,27 @@
  * }
  */
 class Solution {
-    private boolean flag=true;
-    private int heightSubtree(TreeNode root){
-        if(root==null) return 0;
-        int left=heightSubtree(root.left);
-        int right=heightSubtree(root.right);
-        return 1+Math.max(left,right);
-    }
-    public void bfs(TreeNode root, int d){
+    public boolean isCompleteTree(TreeNode root) {
+        if(root==null) return true;
         Queue<TreeNode> q=new LinkedList<>();
         q.offer(root);
-        int h=d;
+        boolean seenNull=false;
         while(!q.isEmpty()){
             int n=q.size();
-            if(h!=0 && n!=Math.pow(2,d-h)){
-                flag=false;
-                return;
-            }
-            boolean isNullBtw=false;
+            
             while(n>0){
                 TreeNode node=q.poll();
-                if(node==null){
-                    isNullBtw=true;
-                    while(!q.isEmpty() && q.peek()==null) q.poll();
-                    break;
+                if(node==null) seenNull=true;
+                else{
+                    if(seenNull) return false;
+                    q.offer(node.left);
+                    q.offer(node.right);
                 }
-                if(node.left!=null) q.offer(node.left);
-                else if(h==2)q.offer(null);
-                if(node.right!=null) q.offer(node.right);
-                else if(h==2) q.offer(null);
                 n--;
 
             }
-            if(isNullBtw && !q.isEmpty()){
-                flag=false;
-                return;
-            }
-            h--;
+            
         }
-    }
-    public boolean isCompleteTree(TreeNode root) {
-        if(root==null) return true;
-        int d=heightSubtree(root);
-        bfs(root,d);
-        return flag;
+        return true;
     }
 }
